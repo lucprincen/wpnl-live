975f5e6d6f3af1b002de93bb3a57872cabad230d
Luc Princen <hi@lucp.nl>
2020-09-03 14:09:55 +0200

    new layout-workshops

---
+++ assets/dist/js/menu.js
@@ -0,0 +1,7 @@
+window.onload = function(){
+    var btn = document.querySelector(".toggle-menu");
+    btn.onclick = function( e ){
+        var menu = document.querySelector(".menu-container")
+        menu.classList.toggle('active');
+    }
+}
